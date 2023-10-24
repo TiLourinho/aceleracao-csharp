064d34d9529b60103a7e6b6d9d07512eaa0076df
Tiago Lourinho <lourinho.tiago@gmail.com>
2023-10-24 16:44:03 -0300

    feat: structure Array

--- Seção 01 - Fundamentos e Recursos da Linguagem C#/Bloco 03 - Arrays, Funções e Exceções/01 - A estrutura Array/Exemplos/Program.cs
+++ Seção 01 - Fundamentos e Recursos da Linguagem C#/Bloco 03 - Arrays, Funções e Exceções/01 - A estrutura Array/Exemplos/Program.cs
@@ -1,1 +1,48 @@
-﻿
+﻿/* A estrutura Array */
+
+// Os Arrays em C# são parecidos com os Arrays em Javascript. O que os diferencia é que, em C#,
+// por se tratar de uma linguagem Fortemente Tipada, os dados são armazenados de apenas um tipo definido previamente
+
+/*
+    <Tipo>[] <Nome do Array>;
+
+    int[] myFirstArray;
+*/
+
+// Inicializando o array com 5 posições
+
+int[] myFirstArray;
+myFirstArray = new int[5];
+
+Console.WriteLine($"[{string.Join(", ", myFirstArray)}]");
+// Quando utilizamos esta forma para instanciar um Array de inteiros, todas as posições do Array iniciam com 0, o valor padrão para o int
+
+// Inicialização direta
+
+int[] mySecondArray = new int[3];
+
+Console.WriteLine($"[{string.Join(", ", mySecondArray)}]");
+
+// Inicialização com valores pré-definidos
+
+int[] myThirdArray = new int[] { 1, 2, 3, 4, 5 };
+
+int[] myFourthArray = { 6, 7, 8, 9, 10 };
+
+Console.WriteLine($"[{string.Join(", ", myThirdArray)}]");
+Console.WriteLine($"[{string.Join(", ", myFourthArray)}]");
+
+// Inicialização para outros tipos
+
+string[] myFifthArray = new string[] { "Vamo", "que", "vamo!" };
+
+string[] mySixthArray = { "Foguete", "não", "tem", "ré!" };
+
+Console.WriteLine($"{string.Join(" ", myFifthArray)}");
+Console.WriteLine($"{string.Join(" ", mySixthArray)}");
+
+// Utilizando Arrays
+
+int[] mySeventhArray = new int[4] { 6, 9, 5, 8 };
+
+Console.WriteLine(mySeventhArray[3]);
