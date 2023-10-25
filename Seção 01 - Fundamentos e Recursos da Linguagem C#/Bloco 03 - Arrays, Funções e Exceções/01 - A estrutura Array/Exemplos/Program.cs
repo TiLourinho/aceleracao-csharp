d3beb70252d7079a394f3de22a68698f5cf16d16
Tiago Lourinho <lourinho.tiago@gmail.com>
2023-10-25 15:55:47 -0300

    feat: multidimensional Array

--- Seção 01 - Fundamentos e Recursos da Linguagem C#/Bloco 03 - Arrays, Funções e Exceções/01 - A estrutura Array/Exemplos/Program.cs
+++ Seção 01 - Fundamentos e Recursos da Linguagem C#/Bloco 03 - Arrays, Funções e Exceções/01 - A estrutura Array/Exemplos/Program.cs
@@ -46,3 +46,42 @@ Console.WriteLine($"{string.Join(" ", mySixthArray)}");
 int[] mySeventhArray = new int[4] { 6, 9, 5, 8 };
 
 Console.WriteLine(mySeventhArray[3]);
+
+/* Arrays multidimensionais */
+
+int[,] multidimensionalArray1 = new int[2, 3];
+
+Console.WriteLine(multidimensionalArray1[1, 2]);
+
+// Inicialização com valores pré-definidos
+
+int[,] multidimensionalArray2 = { { 1, 2, 3 }, { 4, 5, 6 } };
+
+Console.WriteLine(multidimensionalArray2[1, 2]);
+
+// Jagged Array
+
+// Somente instanciamos o array mais externo neste primeiro passo
+// Repare que apenas o primeiro colchetes contém números
+int[][] jaggedArray1 = new int[4][];
+
+// Agora precisamos instanciar um novo array para cada posição do array mais externo
+jaggedArray1[0] = new int[4] { 6, 6, 6, 6 };
+jaggedArray1[1] = new int[3] { 6, 6, 6 };
+jaggedArray1[2] = new int[5] { 6, 6, 6, 6, 6 };
+jaggedArray1[3] = new int[2] { 6, 6 };
+
+// Jagged Array com três dimensionais
+
+int[,,] multiDimensionalArray3 = new int[2, 3, 3];
+
+int[,,] multiDimensionalArray4 = { { { 1, 2 }, { 3, 4 } },{ { 5, 6 }, { 7, 8 } } };
+
+// O array jagged precisa ser instanciado em partes. Primeiro instanciamos o array com 3 dimensões
+int[][][] jaggedArray2 = new int[6][][];
+
+// Depois vamos instanciar a primeira linha do array, recebendo um novo array jagged de 2 dimensões
+jaggedArray2[0] = new int[3][];
+
+// E assim finalmente podemos instanciar a primeira coluna da primeira linha do array com um novo array de 1 dimensão
+jaggedArray2[0][0] = new int[4] { 1, 2, 3, 4 };
