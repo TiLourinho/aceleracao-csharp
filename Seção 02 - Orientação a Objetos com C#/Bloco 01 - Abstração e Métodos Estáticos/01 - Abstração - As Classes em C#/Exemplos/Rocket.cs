042c49c550233af9d21b4526175abddcdc19780f
Tiago Lourinho <lourinho.tiago@gmail.com>
2023-11-27 16:49:21 -0300

    feat: getters and setters

--- Seção 02 - Orientação a Objetos com C#/Bloco 01 - Abstração e Métodos Estáticos/01 - Abstração - As Classes em C#/Exemplos/Rocket.cs
+++ Seção 02 - Orientação a Objetos com C#/Bloco 01 - Abstração e Métodos Estáticos/01 - Abstração - As Classes em C#/Exemplos/Rocket.cs
@@ -3,9 +3,26 @@ namespace Exemplos;
 public class Rocket
 {
     public string? Name { get; set; }
+    public decimal Price { get; set; }
 
-    public Rocket(string name)
+    int _fuel;
+    readonly int _maxFuel = 5000;
+
+    public int FuelAsPercentage
+    {
+        get => _fuel * 100 / _maxFuel;
+        set
+        {
+            if (value > 100 || value < 0)
+                throw new ArgumentOutOfRangeException(_fuel.ToString(), "Value must be between 0 and 100");
+
+            _fuel = value * _maxFuel / 100;
+        }
+    }
+
+    public Rocket(string name, decimal price)
     {
         Name = name;
+        Price = price;
     }
 }
